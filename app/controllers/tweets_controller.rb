cf8bdceb692a535c425dd3a3e878077994f9d6cf
Allison _ <allimacq@gmail.com>
2021-03-26 22:01:07 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -58,10 +58,10 @@ class TweetsController < ApplicationController
     end
   end
   
-  #get "/tweets/:id/delete" do
+  get "/tweets/:id/delete" do
     #if Helpers.logged_in?(session) == true
-      #@tweet = Tweet.find_by_id(params[:id])
-      #erb :'/tweets/delete'
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'/tweets/delete'
     #else
       #redirect "/login"
     #end
